9308bac596a32cea03b8eb0a9004ca3930e55b66
Millie <jay.lee.2280@gmail.com>
2022-06-15 00:46:54 +0900

    [jaypedia/issue-tracker#8] Feat: implement theme switch
    Co-authored-by: ju-kkim <ju-kkim@users.noreply.github.com>

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+
+import { ThemeProvider } from 'styled-components';
+
+import Header from '@/components/Header';
+import ThemeSwitch from '@/components/ThemeSwitch';
+import GlobalStyle from '@/styles/GlobalStyle';
+import { DARK, LIGHT } from '@/styles/theme';
+
 const App = () => {
-  return <div>sample</div>;
+  const [theme, setTheme] = useState(DARK);
+  const isLight = theme === LIGHT;
+
+  const switchTheme = () => {
+    const nextTheme = theme === LIGHT ? DARK : LIGHT;
+    setTheme(nextTheme);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header />
+      <ThemeSwitch switchTheme={switchTheme} isLight={isLight} />
+    </ThemeProvider>
+  );
 };
 
 export default App;

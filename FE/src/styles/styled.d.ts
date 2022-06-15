da6034764ff93103dc6e56ddfd6aabc24e95c360
Millie <jay.lee.2280@gmail.com>
2022-06-15 23:17:09 +0900

    [jaypedia/issue-tracker#7] Design: modify dark & light theme object format
    Co-authored-by: ju-kkim <ju-kkim@users.noreply.github.com>

--- FE/src/styles/styled.d.ts
+++ FE/src/styles/styled.d.ts
@@ -1,11 +1,9 @@
 import 'styled-components';
 
-import { FontType } from '@/styles/font';
 import { ThemeColorsType } from '@/styles/theme';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    font: FontType;
     color: ThemeColorsType;
   }
 }
--- FE/src/styles/theme.ts
+++ FE/src/styles/theme.ts
@@ -1,34 +1,23 @@
 import { color } from './color';
-import { font } from './font';
-
-const defaultTheme = {
-  font,
-};
-
-const darkThemeColors = {
-  headerBg: color.grey[600],
-  bg: color.black,
-  cellBg: color.grey[500],
-  text: color.white,
-  primary: color.primary[200],
-};
-
-const lightThemeColors = {
-  headerBg: color.grey[100],
-  bg: color.white,
-  cellBg: color.white,
-  text: color.black,
-  primary: color.primary[200],
-};
 
 export const DARK = {
-  ...defaultTheme,
-  color: darkThemeColors,
+  color: {
+    headerBg: color.grey[600],
+    bg: color.black,
+    cellBg: color.grey[500],
+    text: color.white,
+    primary: color.primary[200],
+  },
 };
 
 export const LIGHT = {
-  ...defaultTheme,
-  color: lightThemeColors,
+  color: {
+    headerBg: color.grey[100],
+    bg: color.white,
+    cellBg: color.white,
+    text: color.black,
+    primary: color.primary[200],
+  },
 };
 
-export type ThemeColorsType = typeof darkThemeColors;
+export type ThemeColorsType = typeof LIGHT.color;

550198db5394bb7fd4535f19a8014f26b8d3c88a
Millie <jay.lee.2280@gmail.com>
2022-06-15 16:50:19 +0900

    [jaypedia/issue-tracker#7] Design: add properties to theme colors
    Co-authored-by: ju-kkim <ju-kkim@users.noreply.github.com>

--- FE/src/styles/theme.ts
+++ FE/src/styles/theme.ts
@@ -6,15 +6,19 @@ const defaultTheme = {
 };
 
 const darkThemeColors = {
+  headerBg: color.grey[600],
   bg: color.black,
   cellBg: color.grey[500],
   text: color.white,
+  primary: color.primary[200],
 };
 
 const lightThemeColors = {
+  headerBg: color.grey[100],
   bg: color.white,
   cellBg: color.white,
   text: color.black,
+  primary: color.primary[200],
 };
 
 export const DARK = {

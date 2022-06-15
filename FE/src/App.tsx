1ae84c21947a0cd7a1344f88fa0d4f6bf5d41979
Millie <jay.lee.2280@gmail.com>
2022-06-15 17:02:49 +0900

    [jaypedia/issue-tracker#14] Feat: routing using react-router-dom
    Co-authored-by: ju-kkim <ju-kkim@users.noreply.github.com>

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 
-import Header from '@/components/Header';
 import ThemeSwitch from '@/components/ThemeSwitch';
+import Layout from '@/layout';
+import NotFound from '@/pages/NotFound';
 import GlobalStyle from '@/styles/GlobalStyle';
 import { DARK, LIGHT } from '@/styles/theme';
 
@@ -19,7 +21,14 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* TODO: <Route index element={<IssueList />} /> */}
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
       <ThemeSwitch switchTheme={switchTheme} isLight={isLight} />
     </ThemeProvider>
   );

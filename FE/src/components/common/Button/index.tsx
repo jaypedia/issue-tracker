4e16ccb87c09211555ab0f5026ca3dea56af022d
Millie <jay.lee.2280@gmail.com>
2022-06-17 20:36:09 +0900

    [jaypedia/issue-tracker#13] Design: add types to Button & Input components
    Co-authored-by: ju-kkim <ju-kkim@users.noreply.github.com>

--- FE/src/components/common/Button/index.tsx
+++ FE/src/components/common/Button/index.tsx
@@ -3,11 +3,12 @@ import { ButtonStyleProps } from './style';
 
 type ButtonProps = ButtonStyleProps & {
   text: string;
+  type?: 'button' | 'submit' | 'reset' | undefined;
 };
 
-const Button = ({ btnSize, btnColor, text }: ButtonProps) => {
+const Button = ({ btnSize, btnColor, text, type }: ButtonProps) => {
   return (
-    <S.Button btnSize={btnSize} btnColor={btnColor}>
+    <S.Button btnSize={btnSize} btnColor={btnColor} type={type}>
       {text}
     </S.Button>
   );
--- FE/src/components/common/Input/index.tsx
+++ FE/src/components/common/Input/index.tsx
@@ -4,10 +4,11 @@ import { InputStyleProps } from './style';
 type InputProps = InputStyleProps & {
   title: string;
   placeholder: string;
+  type: string;
 };
 
-const Input = ({ inputStyle, title, placeholder }: InputProps) => {
-  return <S.Input title={title} placeholder={placeholder} inputStyle={inputStyle} />;
+const Input = ({ inputStyle, title, placeholder, type }: InputProps) => {
+  return <S.Input type={type} title={title} placeholder={placeholder} inputStyle={inputStyle} />;
 };
 
 export default Input;

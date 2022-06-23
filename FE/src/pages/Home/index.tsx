d8fb5fe66432dd2f49aaa1f45bff6d7cd68c0037
ju-kkim <wndus8198@naver.com>
2022-06-23 19:19:29 +0900

    [geombong/issue-tracker#32] Design: add IssueList to Home page

--- FE/src/pages/Home/index.tsx
+++ FE/src/pages/Home/index.tsx
@@ -1,11 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 import HomeHeader from '@/components/HomeHeader';
+import IssueList from '@/components/IssueList';
 import { InnerContainer, MainWrapper } from '@/styles/common';
 
 const Home = () => {
+  const [issues, setIssues] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getIssues = async () => {
+    setIsLoading(true);
+    const response = await axios.get('/api/issues');
+
+    if (response.data.length) {
+      setIssues(response.data);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getIssues();
+  }, []);
   return (
     <MainWrapper>
       <InnerContainer>
         <HomeHeader />
+        {!isLoading && <IssueList list={issues} />}
       </InnerContainer>
     </MainWrapper>
   );

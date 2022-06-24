0922853c3874da7a1c5bfee90c8f230dcc3cabda
Millie <jay.lee.2280@gmail.com>
2022-06-24 15:50:00 +0900

    [geombong/issue-tracker#47] Feat: add filter utils to mock server

--- FE/src/constants/constants.ts
+++ FE/src/constants/constants.ts
@@ -1,4 +1,16 @@
-export const ISSUE_STATUS = {
+interface IssueStatus {
+  open: 'open';
+  closed: 'closed';
+}
+
+export const ISSUE_STATUS: IssueStatus = {
   open: 'open',
   closed: 'closed',
+};
+
+export const QUERY_KEY = {
+  author: 'author',
+  label: 'label',
+  milestone: 'milestone',
+  assignees: 'assignees',
 };
--- FE/src/pages/Home/index.tsx
+++ FE/src/pages/Home/index.tsx
@@ -6,15 +6,19 @@ import IssueList from '@/components/IssueList';
 import { InnerContainer, MainWrapper } from '@/styles/common';
 
 const Home = () => {
+  // TODO: issue 상태를 전역으로 놓는다
+  // TODO: open/closed 클릭할 때마다 fetch 요청
+  // TODO: open 탭에 있을 때 또 open 탭을 클릭 시에는 fetch 요청 X
+
   const [issues, setIssues] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const getIssues = async () => {
     setIsLoading(true);
-    const response = await axios.get('/api/issues');
+    const response = await axios.get('/api/issues?issueStatus=open');
 
-    if (response.data.length) {
-      setIssues(response.data);
+    if (response.data) {
+      setIssues(response.data.issues);
     }
 
     setIsLoading(false);

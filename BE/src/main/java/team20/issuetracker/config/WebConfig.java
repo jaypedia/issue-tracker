b50a6c836c599d091393558beb310968c7b88cbc
ByeoungJoon Jeon <juni8453@naver.com>
2022-06-27 10:51:40 +0900

    [geombong/issue-tracker#50] Chore: Edit WebConfig DI
    Description
    - 원래 JwtTokenProvider 를 주입받았지만 LoginInterceptor 를 주입받도록 변경

--- BE/src/main/java/team20/issuetracker/config/WebConfig.java
+++ BE/src/main/java/team20/issuetracker/config/WebConfig.java
@@ -1,24 +1,21 @@
 package team20.issuetracker.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import team20.issuetracker.login.interceptor.LoginInterceptor;
-import team20.issuetracker.login.jwt.JwtTokenProvider;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    public WebConfig(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
+    private final LoginInterceptor loginInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
+        registry.addInterceptor(loginInterceptor)
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**", "/", "/index.html", "/login/**");

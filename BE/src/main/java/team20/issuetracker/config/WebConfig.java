afb83b21e5b03178fd3e7eab2e3ebf94095a5cfe
geombong <shoy1415@gmail.com>
2022-06-28 12:00:13 +0900

    [geombong/issue-tracker] Fix: cors fix

--- BE/src/main/java/team20/issuetracker/config/WebConfig.java
+++ BE/src/main/java/team20/issuetracker/config/WebConfig.java
@@ -3,12 +3,14 @@ package team20.issuetracker.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import team20.issuetracker.login.interceptor.LoginInterceptor;
 
 @Configuration
+@EnableWebMvc
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
@@ -25,6 +27,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:9000");
+                .allowedOrigins("http://localhost:9000")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }

ea7af6c2764fab6478332f4905eadf03afc75759
ju-kkim <wndus8198@naver.com>
2022-06-29 16:51:39 +0900

    [geombong/issue-tracker#64] Feat: apply react-query to IssueList

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
@@ -14,6 +15,8 @@ import GlobalStyle from '@/styles/GlobalStyle';
 import { DARK, LIGHT } from '@/styles/theme';
 import { getDefaultTheme } from '@/utils/mode';
 
+const queryClient = new QueryClient();
+
 const App = () => {
   const [theme, setTheme] = useState(getDefaultTheme());
   const isLight = theme === LIGHT;
@@ -24,22 +27,24 @@ const App = () => {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="new-issue" element={<NewIssue />} />
-            <Route path="issue/:id" element={<IssueDetail />} />
-          </Route>
-          <Route path="callback" element={<LoginCallback />} />
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-      <ThemeSwitch switchTheme={switchTheme} isLight={isLight} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="new-issue" element={<NewIssue />} />
+              <Route path="issue/:id" element={<IssueDetail />} />
+            </Route>
+            <Route path="callback" element={<LoginCallback />} />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+        <ThemeSwitch switchTheme={switchTheme} isLight={isLight} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 

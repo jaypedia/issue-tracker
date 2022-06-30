34672b4526de306ad8890bc4d295f76b5a4c7c37
HeechulShin <shoy1415@gmail.com>
2022-06-30 17:07:08 +0900

    [geombong/issue-tracker] Fix: Url Fix

--- BE/src/main/java/team20/issuetracker/login/oauth/controller/OauthController.java
+++ BE/src/main/java/team20/issuetracker/login/oauth/controller/OauthController.java
@@ -1,13 +1,11 @@
 package team20.issuetracker.login.oauth.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
+import lombok.RequiredArgsConstructor;
 import team20.issuetracker.login.oauth.dto.LoginResponse;
 import team20.issuetracker.login.oauth.service.OauthService;
 
@@ -17,13 +15,7 @@ public class OauthController {
 
     private final OauthService oauthService;
 
-    @GetMapping("/login")
-    public RedirectView getToken(RedirectAttributes redirectAttributes) {
-
-        return oauthService.getToken(redirectAttributes);
-    }
-
-    @GetMapping("/login/oauth/github/callback")
+    @GetMapping("/login/oauth/github")
     public ResponseEntity<LoginResponse> login(@RequestParam String code, @RequestParam String state) {
         LoginResponse loginResponse = oauthService.signup(code);
         return ResponseEntity.ok().body(loginResponse);

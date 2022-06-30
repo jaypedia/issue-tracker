b696f0b996dfea4fca9ccd8b359e9f6bab5c9df1
ju-kkim <wndus8198@naver.com>
2022-06-30 17:55:59 +0900

    [geombong/issue-tracker#13] Design: update Loading component UI

--- FE/src/components/common/Loading/index.tsx
+++ FE/src/components/common/Loading/index.tsx
@@ -1,7 +1,17 @@
 import * as S from './style';
 
 const Loading = () => {
-  return <S.Loading>Loading...</S.Loading>;
+  return (
+    <S.LoadingContainer>
+      <S.Text>L</S.Text>
+      <S.Text>o</S.Text>
+      <S.Text>a</S.Text>
+      <S.Text>d</S.Text>
+      <S.Text>i</S.Text>
+      <S.Text>n</S.Text>
+      <S.Text>g</S.Text>
+    </S.LoadingContainer>
+  );
 };
 
 export default Loading;

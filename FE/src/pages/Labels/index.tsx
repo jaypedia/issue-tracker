436f6665d582f6555f026d54ab911a9ab59f52d5
Millie <jay.lee.2280@gmail.com>
2022-07-11 14:41:05 +0900

    [geombong/issue-tracker#68] Design: implement `Labels` page UI

--- FE/src/pages/Labels/index.tsx
+++ FE/src/pages/Labels/index.tsx
@@ -1,9 +1,48 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
 import * as S from './style';
 
+import Label from '@/components/common/Label';
 import Navbar from '@/components/Navbar';
+import { MainWrapper, InnerContainer } from '@/styles/common';
+import { ListHeader, ListContainer, Item } from '@/styles/list';
+
+// TODO: 반복되는 fetch 로직 Refactoring
+const fetchAPI = () => {
+  return axios.get(`/api/labels`);
+};
 
 const Labels = () => {
-  return <Navbar btnText="New Label" />;
+  const { data, isLoading } = useQuery('labels', fetchAPI);
+
+  return (
+    <MainWrapper>
+      <InnerContainer>
+        <Navbar btnText="New Label" />
+        <ListContainer>
+          <ListHeader type="large">{data?.data.labelCount} Labels</ListHeader>
+          {data?.data.labels.map(({ title, backgroundColor, textColor, description }, i) => (
+            <Item key={i} type="large">
+              <S.LabelWrapper>
+                <Label
+                  size="small"
+                  title={title}
+                  backgroundColor={backgroundColor}
+                  textColor={textColor}
+                />
+              </S.LabelWrapper>
+              <S.Description>{description}</S.Description>
+              <S.ButtonsWrapper>
+                <S.TextButton>Edit</S.TextButton>
+                <S.TextButton>Delete</S.TextButton>
+              </S.ButtonsWrapper>
+            </Item>
+          ))}
+        </ListContainer>
+      </InnerContainer>
+    </MainWrapper>
+  );
 };
 
 export default Labels;
--- FE/src/pages/Labels/style.ts
+++ FE/src/pages/Labels/style.ts
@@ -1,1 +1,32 @@
 import styled from 'styled-components';
+
+import { FONT_MIXIN, mixins } from '@/styles/mixins';
+
+const LabelWrapper = styled.div`
+  width: 500px;
+`;
+
+const Description = styled.p`
+  width: 100%;
+  color: ${({ theme: { color } }) => color.lightText};
+  ${FONT_MIXIN.xSmall(400)};
+`;
+
+const TextButton = styled.button`
+  :hover {
+    color: ${({ theme: { color } }) => color.primary[200]};
+    text-decoration: underline;
+  }
+`;
+
+const ButtonsWrapper = styled.div`
+  ${FONT_MIXIN.xSmall(400)};
+  ${mixins.flexBox({ justifyContent: 'flex-end' })}
+  color: ${({ theme: { color } }) => color.lightText};
+
+  & > :first-child {
+    margin-right: 10px;
+  }
+`;
+
+export { LabelWrapper, Description, TextButton, ButtonsWrapper };

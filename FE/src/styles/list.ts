c7ca8e6ff91658c915f6ca695f3672264a31e74a
Millie <jay.lee.2280@gmail.com>
2022-07-12 16:47:38 +0900

    [geombong/issue-tracker#69] Design: implement `Milestone` page UI

--- FE/src/styles/list.ts
+++ FE/src/styles/list.ts
@@ -3,22 +3,22 @@ import styled from 'styled-components';
 import { FONT_MIXIN, mixins } from '@/styles/mixins';
 
 type ListStyleType = {
-  type?: 'large';
+  type?: 'large' | 'milestone';
 };
 
 const ListHeader = styled.div<ListStyleType>`
   ${FONT_MIXIN.small(400)}
   ${mixins.flexBox({ justifyContent: 'space-between' })};
-  padding: ${({ type }) => (type === 'large' ? '15px 20px' : '15px')};
-  color: ${({ theme: { color }, type }) =>
-    type === 'large' ? color.text : color.cell.font.initial};
+  padding: ${({ type }) => (type ? '15px 20px' : '15px')};
+  color: ${({ theme: { color }, type }) => (type ? color.text : color.cell.font.initial)};
   background: ${({ theme: { color } }) => color.cell.bg.cellHeaderBg};
 `;
 
 const Item = styled.li<ListStyleType>`
   ${mixins.flexBox({ justifyContent: 'space-between' })}
+  ${({ type }) => type === 'milestone' && 'align-items: flex-start;'}
   border-top: 1px solid ${({ theme: { color } }) => color.line};
-  padding: ${({ type }) => (type === 'large' ? '15px 20px' : '5px 15px')};
+  padding: ${({ type }) => (type ? '15px 20px' : '5px 15px')};
 
   :hover {
     background-color: ${({ theme: { color } }) => color.cell.bg.cellHeaderBg};

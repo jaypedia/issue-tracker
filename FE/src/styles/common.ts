9e5296b841d736b2db416b7fc156421072d0b9b5
Millie <jay.lee.2280@gmail.com>
2022-07-12 16:48:26 +0900

    [geombong/issue-tracker#7] Design: add `Flex` to common style

--- FE/src/styles/common.ts
+++ FE/src/styles/common.ts
@@ -20,18 +20,22 @@ const ColumnWrapper = styled(InnerContainer)`
 const headerWrapperStyle = css`
   width: 100%;
   border-bottom: 1px solid ${({ theme: { color } }) => color.line};
-  padding: 32px 0;
-  margin-bottom: 30px;
+  padding: 15px 0;
+  margin-bottom: 35px;
 `;
 
 const Heading1 = styled.h1`
-  font-size: ${FONT_MIXIN.display(400)};
+  ${FONT_MIXIN.display(400)};
   margin: 0;
 `;
 
 const FlexBetween = styled.div`
   width: 100%;
   ${mixins.flexBox({ justifyContent: 'space-between' })}
+`;
+
+const Flex = styled.div`
+  ${mixins.flexBox({})}
 `;
 
 const FlexStart = styled.div`
@@ -50,6 +54,7 @@ export {
   ColumnWrapper,
   headerWrapperStyle,
   Heading1,
+  Flex,
   FlexBetween,
   FlexStart,
   FlexColumn,

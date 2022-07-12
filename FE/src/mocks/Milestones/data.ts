c052962f212843d4d2f69eb39e115003a4a4f529
Millie <jay.lee.2280@gmail.com>
2022-07-12 16:37:48 +0900

    [geombong/issue-tracker#47] Feat: add Milestones mock data

---
+++ FE/src/mocks/Milestones/data.ts
@@ -0,0 +1,34 @@
+export const mockMilestones = {
+  allMileStonesCount: 3,
+  openMileStonesCount: 2,
+  closedMileStonesCount: 1,
+  milestones: [
+    {
+      id: 1,
+      title: '[BE] New Week 1',
+      dueDate: '2022-07-20 00:00:00',
+      updatedAt: '2022-07-11 02:58:22',
+      description: 'Make API',
+      openIssueCount: 5,
+      closedIssueCount: 2,
+    },
+    {
+      id: 2,
+      title: '[FE] Week 2',
+      dueDate: '2022-07-03 00:00:00',
+      updatedAt: '2022-07-12 05:58:22',
+      description: 'Common Components',
+      openIssueCount: 1,
+      closedIssueCount: 8,
+    },
+    {
+      id: 3,
+      title: '[FE] Week 4',
+      dueDate: '2022-07-20 00:00:00',
+      updatedAt: '2022-07-11 02:58:22',
+      description: 'Learn Recoil & React-query',
+      openIssueCount: 4,
+      closedIssueCount: 9,
+    },
+  ],
+};

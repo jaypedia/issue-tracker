00acc96e6f01f03925ab018695e62c3f7d883e91
Millie <jay.lee.2280@gmail.com>
2022-07-12 16:43:16 +0900

    [geombong/issue-tracker#69] Design: add more props to `ProgressBar` component

--- FE/src/components/common/ProgressBar/index.tsx
+++ FE/src/components/common/ProgressBar/index.tsx
@@ -1,11 +1,24 @@
 import * as S from './style';
-import { ProgressBarProps } from './type';
+import { ProgressBarProps, ProgressBarInfoProps } from './type';
 
 // TODO: Milestone Link to 해당하는 Milestone으로 이동되도록 변경
-const ProgressBarInfo = ({ size }: ProgressBarProps): JSX.Element => {
+const ProgressBarInfo = ({
+  size,
+  openIssueCount,
+  closedIssueCount,
+  percent,
+}: ProgressBarInfoProps): JSX.Element => {
   switch (size) {
     case 'large':
-      return <S.MilestoneStats>50% complete 2 open 7 closed</S.MilestoneStats>;
+      return (
+        <S.MilestoneStats>
+          <p>{percent}% complete</p>
+          <div>
+            <S.MilestoneLink to="/">{openIssueCount} open</S.MilestoneLink>
+            <S.MilestoneLink to="/">{closedIssueCount} closed</S.MilestoneLink>
+          </div>
+        </S.MilestoneStats>
+      );
     case 'small':
       return <S.MilestoneLink to="/">Week 4</S.MilestoneLink>;
     default:
@@ -13,13 +26,20 @@ const ProgressBarInfo = ({ size }: ProgressBarProps): JSX.Element => {
   }
 };
 
-const ProgressBar = ({ size }: ProgressBarProps) => {
+const ProgressBar = ({ size, openIssueCount, closedIssueCount }: ProgressBarProps) => {
+  const percent = Math.floor((closedIssueCount / (openIssueCount + closedIssueCount)) * 100);
+
   return (
     <S.ProgressBarWrapper size={size}>
       <S.ProgressBack size={size}>
-        <S.ProgressItem size={size} />
+        <S.ProgressItem size={size} percent={percent} />
       </S.ProgressBack>
-      <ProgressBarInfo size={size} />
+      <ProgressBarInfo
+        size={size}
+        openIssueCount={openIssueCount}
+        closedIssueCount={closedIssueCount}
+        percent={percent}
+      />
     </S.ProgressBarWrapper>
   );
 };
--- FE/src/components/common/ProgressBar/style.ts
+++ FE/src/components/common/ProgressBar/style.ts
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import styled, { css } from 'styled-components';
 
-import { ProgressBarProps } from './type';
+import { ProgressBarOptional } from './type';
 
 import { COLOR } from '@/styles/color';
+import { FlexBetween } from '@/styles/common';
 import { FONT_MIXIN, mixins } from '@/styles/mixins';
 
-const ProgressStyle = css<ProgressBarProps>`
+const ProgressStyle = css<ProgressBarOptional>`
   height: ${({ size }) => (size === 'large' ? '12px' : '8px')};
   border-radius: 6px;
 `;
@@ -16,27 +17,31 @@ const ProgressBack = styled.div`
   width: 100%;
   overflow: hidden;
   background-color: ${COLOR.transparent[100]};
+  margin-bottom: 5px;
 `;
 
 const ProgressItem = styled.div`
   ${ProgressStyle}
-  width: 50%;
+  width: ${({ percent }) => `${percent}%`};
   background: linear-gradient(to right, ${COLOR.primary[100]}, ${COLOR.primary[200]});
 `;
 
 const MilestoneLink = styled(Link)`
-  ${FONT_MIXIN.xSmall(700)}
-  margin-top: 10px;
+  ${FONT_MIXIN.xSmall(500)}
+  margin-left: 10px;
+
+  :hover {
+    color: ${COLOR.primary[300]};
+  }
 `;
 
 // TODO: 추후 open, close 버튼 클릭 시 Filtering 기능 추가, font-size 추가
-const MilestoneStats = styled.div`
+const MilestoneStats = styled(FlexBetween)`
   font-size: 14px;
-  margin-top: 8px;
 `;
 
-const ProgressBarWrapper = styled.div<ProgressBarProps>`
-  ${({ size }) => size === 'large' && 'width: 60%'};
+const ProgressBarWrapper = styled.div<ProgressBarOptional>`
+  ${({ size }) => size === 'large' && 'width: 100%'};
   ${mixins.flexBox({ direction: 'column', alignItems: 'flex-start' })};
 `;
 

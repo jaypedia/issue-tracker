ed180eed0de563feef7d462fc91e1be932ccd518
ByeoungJoon Jeon <juni8453@naver.com>
2022-07-19 17:19:06 +0900

    [geombong/issue-tracker#99] Feat: Milestone Delete API
    ## Description
    - Milestone Delete API 개발

--- BE/src/main/java/team20/issuetracker/controller/MilestoneController.java
+++ BE/src/main/java/team20/issuetracker/controller/MilestoneController.java
@@ -2,35 +2,38 @@ package team20.issuetracker.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team20.issuetracker.domain.milestone.request.SaveMilestoneDto;
-import team20.issuetracker.domain.milestone.response.ReadMilestoneDto;
-import team20.issuetracker.domain.milestone.response.ReadMilestoneListDto;
+import team20.issuetracker.domain.milestone.response.MilestoneDto;
+import team20.issuetracker.domain.milestone.response.ResponseMilestone;
 import team20.issuetracker.service.MilestoneService;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@RequestMapping("/milestones")
 @RestController
 public class MilestoneController {
 
     private final MilestoneService milestoneService;
 
-    @PostMapping("/milestones")
+    @PostMapping
     public ResponseEntity<Long> save(@RequestBody SaveMilestoneDto saveMilestoneDto) {
         Long mileStoneId = milestoneService.save(saveMilestoneDto);
 
         return ResponseEntity.ok(mileStoneId);
     }
 
-    @GetMapping("/milestones")
-    public ResponseEntity<ReadMilestoneListDto> read() {
-        List<ReadMilestoneDto> readMilestoneDto = milestoneService.findAll();
-        ReadMilestoneListDto readMilestoneListDto = milestoneService.getAllMilestoneData(readMilestoneDto);
+    @GetMapping
+    public ResponseEntity<ResponseMilestone> read() {
+        List<MilestoneDto> milestoneDto = milestoneService.findAll();
+        ResponseMilestone responseMilestone = milestoneService.getAllMilestoneData(milestoneDto);
 
-        return ResponseEntity.ok(readMilestoneListDto);
+        return ResponseEntity.ok(responseMilestone);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        milestoneService.delete(id);
     }
 }
--- BE/src/main/java/team20/issuetracker/service/MilestoneService.java
+++ BE/src/main/java/team20/issuetracker/service/MilestoneService.java
@@ -7,11 +7,12 @@ import team20.issuetracker.domain.milestone.Milestone;
 import team20.issuetracker.domain.milestone.MilestoneRepository;
 import team20.issuetracker.domain.milestone.MilestoneStatus;
 import team20.issuetracker.domain.milestone.request.SaveMilestoneDto;
-import team20.issuetracker.domain.milestone.response.ReadMilestoneDto;
-import team20.issuetracker.domain.milestone.response.ReadMilestoneListDto;
+import team20.issuetracker.domain.milestone.response.MilestoneDto;
+import team20.issuetracker.domain.milestone.response.ResponseMilestone;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -33,18 +34,26 @@ public class MilestoneService {
     }
 
     @Transactional(readOnly = true)
-    public List<ReadMilestoneDto> findAll() {
+    public List<MilestoneDto> findAll() {
         List<Milestone> findMilestones = milestoneRepository.findAll();
 
         return findMilestones.stream()
-                .map(ReadMilestoneDto::of).collect(Collectors.toList());
+                .map(MilestoneDto::of).collect(Collectors.toList());
     }
 
-    public ReadMilestoneListDto getAllMilestoneData(List<ReadMilestoneDto> milestones) {
+    public ResponseMilestone getAllMilestoneData(List<MilestoneDto> milestones) {
         int allMilestoneCount = milestones.size();
         long openMilestonesCount = milestones.stream().filter(milestone -> milestone.getMilestoneStatus().equals(MilestoneStatus.OPEN)).count();
         long closeMilestonesCount = milestones.stream().filter(milestone -> milestone.getMilestoneStatus().equals(MilestoneStatus.CLOSE)).count();
 
-        return ReadMilestoneListDto.of(allMilestoneCount, openMilestonesCount, closeMilestonesCount, milestones);
+        return ResponseMilestone.of(allMilestoneCount, openMilestonesCount, closeMilestonesCount, milestones);
+    }
+
+    @Transactional(readOnly = true)
+    public void delete(Long id) {
+        Milestone findMilestone = milestoneRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("해당 Milestone 은 존재하지 않습니다."));
+
+        milestoneRepository.delete(findMilestone);
     }
 }

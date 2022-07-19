b70d81ededdc6b97cad9c7bc2d4183b8e9f1edc7
ByeoungJoon Jeon <juni8453@naver.com>
2022-07-19 19:48:23 +0900

    [geombong/issue-tracker#99] Test: add Milestone Read, Update, Delete Test
    ## Description
    1. MilestoneReadTest
      - 생성 후 List 를 불러왔을 떄 size 테스트
    2. MilestoneUpdateTest
      - 생성 후 Update 했을 때 Return 되는 Id 테스트
      - 생성 후 Update 했을 떄 바뀌지 않는 값 테스트
    3. MilestoneDeleteTest
      - 2개의 Milestone 생성 후 delete 하고 size 테스트

--- BE/src/main/java/team20/issuetracker/domain/milestone/request/UpdateMilestoneDto.java
+++ BE/src/main/java/team20/issuetracker/domain/milestone/request/UpdateMilestoneDto.java
@@ -2,11 +2,13 @@ package team20.issuetracker.domain.milestone.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.Setter;
 import team20.issuetracker.domain.milestone.MilestoneStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 public class UpdateMilestoneDto {
 
     private String title;

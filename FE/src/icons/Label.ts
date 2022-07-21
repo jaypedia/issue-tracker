89d34128d37ee161a6fa3ec28cd14a9e5200cae1
Millie <jay.lee.2280@gmail.com>
2022-07-21 16:28:35 +0900

    [geombong/issue-tracker#7] Design: add `Change` icon

--- FE/src/icons/Label.ts
+++ FE/src/icons/Label.ts
@@ -1,7 +1,13 @@
 import { GoTag } from 'react-icons/go';
+import { TiArrowSync } from 'react-icons/ti';
 import styled from 'styled-components';
 
 export const Label = styled(GoTag)`
   width: 16px;
   height: 16px;
+`;
+
+export const Change = styled(TiArrowSync)`
+  width: 20px;
+  height: 20px;
 `;

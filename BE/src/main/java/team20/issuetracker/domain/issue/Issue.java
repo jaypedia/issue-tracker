02d3bed77bb03ada7772bdb1b261c7b2247139b6
HeechulShin <shoy1415@gmail.com>
2022-07-21 13:05:19 +0900

    [geombong/issue-tracker#112] Feat: removeAutoGeneratedFields
    Description
    - Auditing으로 자동 생성되는 필드값 Dto와 엔티티에서 제거
    - 자동생성되는 공통 필드 값 추출

--- BE/src/main/java/team20/issuetracker/domain/issue/Issue.java
+++ BE/src/main/java/team20/issuetracker/domain/issue/Issue.java
@@ -1,37 +1,42 @@
 package team20.issuetracker.domain.issue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import team20.issuetracker.domain.AuditingFields;
 import team20.issuetracker.domain.assginee.Assignee;
 import team20.issuetracker.domain.comment.Comment;
 import team20.issuetracker.domain.label.Label;
 import team20.issuetracker.domain.milestone.Milestone;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.*;
-
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Issue {
+public class Issue extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String author;
     private String title;
     private String content;
 
     @Enumerated(value = EnumType.STRING)
     private IssueStatus status = IssueStatus.OPEN;
-
-    private LocalDateTime createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "milestone_id")
@@ -46,16 +51,14 @@ public class Issue {
     @OneToMany(mappedBy = "issue")
     private List<IssueLabel> issueLabels = new ArrayList<>();
 
-    private Issue(String author, String title, String content, LocalDateTime createdAt, Milestone milestone) {
-        this.author = author;
+    private Issue(String title, String content, Milestone milestone) {
         this.title = title;
         this.content = content;
-        this.createdAt = createdAt;
         this.milestone = milestone;
     }
 
-    public static Issue of(String author, String title, String content, LocalDateTime createdAt, Milestone milestone) {
-        return new Issue(author, title, content, createdAt, milestone);
+    public static Issue of(String title, String content, Milestone milestone) {
+        return new Issue(title, content, milestone);
     }
 
     public void addAssignees(List<Assignee> assignees) {

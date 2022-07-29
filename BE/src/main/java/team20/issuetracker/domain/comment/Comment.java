8ecbb5279df1ad2633dfadb8f2a58878bc125b24
geombong <shoy1415@gmail.com>
2022-07-29 11:23:14 +0900

    [geombong/issue-tracker##118] Feat: Edit Comment Entity Filed Value
    Description
    - Comment Create API 개발을 위해 Comment Entity 필드 값 변경

--- BE/src/main/java/team20/issuetracker/domain/comment/Comment.java
+++ BE/src/main/java/team20/issuetracker/domain/comment/Comment.java
@@ -1,30 +1,48 @@
 package team20.issuetracker.domain.comment;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team20.issuetracker.domain.AuditingFields;
 import team20.issuetracker.domain.issue.Issue;
-
-import java.time.LocalDateTime;
-
-import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Comment {
+public class Comment extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDateTime createdAt;
-
+    private String author;
+    private String image;
     @Lob
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "issue_id")
     private Issue issue;
+
+
+    private Comment(String author, String image, String content, Issue issue) {
+        this.author = author;
+        this.image = image;
+        this.content = content;
+        this.issue = issue;
+    }
+
+    public static Comment of(String memberId, String profileImageUrl, String content, Issue issue) {
+        return new Comment(memberId, profileImageUrl, content, issue);
+    }
 }
 

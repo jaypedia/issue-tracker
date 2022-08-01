3049e1387a86178b5fdbe945b6e0de9fbb01b891
ByeoungJoon Jeon <juni8453@naver.com>
2022-08-01 12:06:48 +0900

    [geombong/issue-tracker#126] Feat: Add RequestSaveIssueDto Validation
    ## description
    - title, content 에 @NotEmpty, @Size 로 Validation 추가

--- BE/src/main/java/team20/issuetracker/service/dto/request/RequestSaveIssueDto.java
+++ BE/src/main/java/team20/issuetracker/service/dto/request/RequestSaveIssueDto.java
@@ -6,13 +6,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class RequestSaveIssueDto {
 
+    @NotEmpty
+    @Size(max = 50, message = "Issue 의 제목은 50글자를 넘을 수 없습니다.")
     private String title;
+
+    @NotEmpty
+    @Size(max = 800, message = "Issue 의 본문은 800글자를 넘을 수 없습니다.")
     private String content;
+
     private List<Long> assigneeIds;
     private List<Long> labelIds;
     private Long milestoneId;

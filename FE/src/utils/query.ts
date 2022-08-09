b7f9cbdc478be65909104d6544b0535effbcab2e
Millie <jay.lee.2280@gmail.com>
2022-08-09 11:50:39 +0900

    [geombong/issue-tracker#27] Refactor: `fetchAPI` function

--- FE/src/utils/query.ts
+++ FE/src/utils/query.ts
@@ -1,13 +1,32 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
+import { API } from '@/constants/api';
 import { IssueStatusType, IssueDataType } from '@/types/issueTypes';
+import { LabelDataType } from '@/types/labelTypes';
 
-export const fetchIssue = async <T>(issueStatus: IssueStatusType): Promise<T> => {
-  const response = await axios.get(`/api/issues?issueStatus=${issueStatus}`);
-  return response.data;
+type API = 'issues' | 'labels' | 'milestones';
+
+export const fetchAPI = async <T>(apiName: API, params?: object): Promise<T | undefined> => {
+  try {
+    const response = await axios.get(`/api/${apiName}`, { params });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 };
 
 export const useIssueQuery = (issueStatus: IssueStatusType) => {
-  return useQuery(['issueList', issueStatus], () => fetchIssue<IssueDataType>(issueStatus));
+  return useQuery(['issueList', issueStatus], () =>
+    fetchAPI<IssueDataType>(API.ISSUES, { params: issueStatus }),
+  );
+};
+
+export const useLabelQuery = () => {
+  return useQuery([API.LABELS], () => fetchAPI<LabelDataType>(API.LABELS));
+};
+
+export const useMilestoneQuery = () => {
+  return useQuery([API.MILESTONES], () => fetchAPI<LabelDataType>(API.MILESTONES));
 };

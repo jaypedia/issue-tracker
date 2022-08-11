b35ad52e9fa710f6f0e38fac5563282d8a3fca8c
Millie <jay.lee.2280@gmail.com>
2022-08-11 10:42:15 +0900

    [geombong/issue-tracker#13] Design: `TextArea` component form milestone page

--- FE/src/components/common/TextArea/index.tsx
+++ FE/src/components/common/TextArea/index.tsx
@@ -2,20 +2,36 @@ import * as S from './style';
 
 type TextAreaProps = {
   name: string;
+  usage: 'comment' | 'milestone';
+  textareaLabel?: string;
 };
 
-const TextArea = ({ name }: TextAreaProps) => {
-  return (
-    <S.TextAreaWrapper>
-      <S.TextArea placeholder="Leave a comment" name={name} />
-      <S.InputFile
-        accept=".gif,.jpeg,.jpg,.mov,.mp4,.png,.svg,.csv,.docx,.fodg,.fodp,.fods,.fodt,.gz,.log,.md,.odf,.odg,.odp,.ods,.odt,.pdf,.pptx,.txt,.xls,.xlsx,.zip"
-        type="file"
-        id="inputFile"
-      />
-      <S.InputLabel htmlFor="inputFile">Attach files by selecting them.</S.InputLabel>
-    </S.TextAreaWrapper>
-  );
+const TextArea = ({ name, usage, textareaLabel }: TextAreaProps) => {
+  switch (usage) {
+    case 'comment':
+      return (
+        <S.TextAreaWrapper>
+          <S.TextArea placeholder="Leave a comment" name={name} usage={usage} />
+          <S.InputFile
+            accept=".gif,.jpeg,.jpg,.mov,.mp4,.png,.svg,.csv,.docx,.fodg,.fodp,.fods,.fodt,.gz,.log,.md,.odf,.odg,.odp,.ods,.odt,.pdf,.pptx,.txt,.xls,.xlsx,.zip"
+            type="file"
+            id="inputFile"
+          />
+          <S.InputLabel htmlFor="inputFile">Attach files by selecting them.</S.InputLabel>
+        </S.TextAreaWrapper>
+      );
+    case 'milestone':
+      return (
+        <>
+          <S.TextAreaLabel htmlFor="description">{textareaLabel}</S.TextAreaLabel>
+          <S.TextAreaWrapper>
+            <S.TextArea id="description" name={name} placeholder="Description" usage={usage} />
+          </S.TextAreaWrapper>
+        </>
+      );
+    default:
+      return null;
+  }
 };
 
 export default TextArea;
--- FE/src/components/common/TextArea/style.ts
+++ FE/src/components/common/TextArea/style.ts
@@ -1,6 +1,7 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { largeStyle, inputCommonStyle } from '@/components/common/Input/style';
+import { FONT_MIXIN } from '@/styles/mixins';
 
 const TextAreaWrapper = styled.div`
   width: 100%;
@@ -9,14 +10,23 @@ const TextAreaWrapper = styled.div`
   overflow: hidden;
 `;
 
-const TextArea = styled.textarea`
-  ${largeStyle}
-  ${inputCommonStyle}
-  height: 340px;
-  resize: vertical;
+const commentStyle = css`
   border-bottom: 1px dashed ${({ theme: { color } }) => color.line};
   border-bottom-right-radius: unset;
   border-bottom-left-radius: unset;
+  height: 340px;
+`;
+
+type TextAreaStyle = {
+  usage: 'comment' | 'milestone';
+};
+
+const TextArea = styled.textarea<TextAreaStyle>`
+  ${largeStyle}
+  ${inputCommonStyle}
+  resize: vertical;
+  height: 200px;
+  ${({ usage }) => usage === 'comment' && commentStyle}
 `;
 
 const InputFile = styled.input`
@@ -30,4 +40,8 @@ const InputLabel = styled.label`
   padding: 18px 24px;
 `;
 
-export { TextAreaWrapper, TextArea, InputFile, InputLabel };
+const TextAreaLabel = styled.label`
+  ${FONT_MIXIN.xSmall(700)}
+`;
+
+export { TextAreaWrapper, TextArea, InputFile, InputLabel, TextAreaLabel };

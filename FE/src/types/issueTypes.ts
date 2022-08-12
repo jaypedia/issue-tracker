d1f2afc0a126f24a5a515c5ba2b5be7eeeecdb05
Millie <jay.lee.2280@gmail.com>
2022-08-12 14:50:35 +0900

    [geombong/issue-tracker#32] Refactor: `IssueDetailInfo` component & issueTypes

--- FE/src/types/issueTypes.ts
+++ FE/src/types/issueTypes.ts
@@ -1,9 +1,38 @@
-import { IssueItem } from '@/components/IssueList/type';
+import { ILabel } from '@/types/labelTypes';
+import { MilestoneType } from '@/types/milestoneTypes';
 
 export type IssueStatusType = 'open' | 'closed';
 
-export interface IssueDataType {
-  issues: IssueItem[];
-  OpenIssueCount: number;
-  ClosedIssueCount: number;
+export type Assignee = {
+  id: number;
+  userId: string;
+  image: string;
+};
+
+type Comment = {
+  id: number;
+  author: string;
+  image: string;
+  content: string;
+  createdAt: string;
+};
+
+export type IssueType = {
+  id: number;
+  issueTitle: string;
+  author: string;
+  createdAt: string;
+  image: string;
+  commentCount: number;
+  content: string;
+  milestone: MilestoneType;
+  comments: Comment[];
+  labels: ILabel[];
+  assignees: Assignee[];
+  issueStatus: IssueStatusType;
+};
+export interface IssuesDataType {
+  openIssueCount: number;
+  closedIssueCount: number;
+  issues: IssueType[] | [];
 }

f5ce65f94b679578240ddf84dfa055d2ba7ded06
Millie <jay.lee.2280@gmail.com>
2022-08-14 12:30:16 +0900

    [geombong/issue-tracker#29] Feat: implement postComment api

--- FE/src/apis/issueApi.ts
+++ FE/src/apis/issueApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { IssueType } from '@/types/issueTypes';
+import { IssueType, IssueEditDataType, CommentDataType } from '@/types/issueTypes';
 
 export const getIssueDetail = async <T>(id: number): Promise<T | undefined> => {
   try {
@@ -20,9 +20,17 @@ export const postIssue = async (issueData: IssueType) => {
   }
 };
 
-export const patchIssue = async (id: number, issueData: IssueType) => {
+export const patchIssue = async (id: number, issueData: IssueEditDataType | CommentDataType) => {
   try {
     axios.post(`/api/issues/${id}`, issueData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postComment = async (id: number, commentData: CommentDataType) => {
+  try {
+    axios.post(`/api/issues/${id}/comment`, commentData);
   } catch (error) {
     console.log(error);
   }

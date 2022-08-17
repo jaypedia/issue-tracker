90de31744b91a1d8928bff509ed43ff96f7942a7
Millie <jay.lee.2280@gmail.com>
2022-08-17 16:39:05 +0900

    [geombong/issue-tracker#27] Feat: apply checkedItemCount to IssueListHeader

--- FE/src/components/IssueList/IssueListHeader/index.tsx
+++ FE/src/components/IssueList/IssueListHeader/index.tsx
@@ -1,62 +1,40 @@
+import HeaderDropDown from './HeaderDropDown';
 import * as S from './style';
 import TabBar from './TabBar';
 
 import CheckBox from '@/components/common/CheckBox';
-import DropDown from '@/components/common/DropDown';
-import { mockAssignees } from '@/mocks/Assignees/data';
-import { mockLabels } from '@/mocks/Labels/data';
-import { mockMilestones } from '@/mocks/Milestones/data';
+import { HeaderDropDownList } from '@/constants/issueDropDown';
+import useCheckBox from '@/hooks/useCheckBox';
 import { ListHeader } from '@/styles/list';
 
 type IssueListHeader = {
-  openIssueCount?: number;
-  closedIssueCount?: number;
+  openIssueCount: number;
+  closedIssueCount: number;
 };
-
-const authorList = {
-  indicator: 'Author',
-  title: 'Filter by author',
-  menus: mockAssignees,
-};
-
-const labelList = {
-  indicator: 'Label',
-  title: 'Filter by label',
-  menus: mockLabels.labels,
-};
-
-const milestoneList = {
-  indicator: 'Milestone',
-  title: 'Filter by milestone',
-  menus: mockMilestones.milestones,
-};
-
-const assingeeList = {
-  indicator: 'Assignee',
-  title: 'Filter by who’s assigned',
-  menus: mockAssignees,
-};
-
-const HeaderDropDownList = [authorList, labelList, milestoneList, assingeeList];
 
 const IssueListHeader = ({ openIssueCount, closedIssueCount }: IssueListHeader) => {
+  const { isAllChecked, checkedItems, toggleAllCheckBoxes } = useCheckBox();
+  const checkedItemCount = checkedItems.size;
+
   return (
     <ListHeader>
       <S.Flex>
-        <CheckBox id="headerCheckBox" isHeader />
-        <TabBar openIssueCount={openIssueCount} closedIssueCount={closedIssueCount} />
+        <CheckBox
+          id="headerCheckBox"
+          isHeader
+          onChange={toggleAllCheckBoxes}
+          checked={isAllChecked}
+        />
+        {checkedItemCount > 0 ? (
+          `${checkedItemCount} selected`
+        ) : (
+          <TabBar openIssueCount={openIssueCount} closedIssueCount={closedIssueCount} />
+        )}
       </S.Flex>
       <S.ListFilter>
         {HeaderDropDownList.map(list => (
           <S.ListFilterItem>
-            <DropDown
-              indicatorType="small"
-              indicatorTitle={list.indicator}
-              menuPosition="right"
-              detailsMenuList={list}
-              hasCheckBox
-              checkType="checkBox"
-            />
+            <HeaderDropDown list={list} />
           </S.ListFilterItem>
         ))}
       </S.ListFilter>

7f448e27b2ee835a8c9142ef4b7e39e0f05c8bc8
geombong <shoy1415@gmail.com>
2022-08-24 22:04:52 +0900

    [geombong/issue-tracker#168] Feat: Add author image when looking up issues
    Description
    - 이슈 조회 시 작성자의 이미지가 나타나도록 이미지 추가

--- BE/src/main/java/team20/issuetracker/service/dto/response/ResponseIssueDto.java
+++ BE/src/main/java/team20/issuetracker/service/dto/response/ResponseIssueDto.java
@@ -8,10 +8,10 @@ import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 import team20.issuetracker.domain.issue.Issue;
 import team20.issuetracker.domain.issue.IssueAssignee;
 import team20.issuetracker.domain.issue.IssueLabel;
+import team20.issuetracker.domain.member.Member;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -20,6 +20,7 @@ public class ResponseIssueDto {
     private Long id;
     private String title;
     private String author;
+    private String image;
     private int commentCount;
     private String content;
     private LocalDate createdAt;
@@ -29,7 +30,61 @@ public class ResponseIssueDto {
     private Set<ResponseLabelDto> labels;
     private Set<ResponseAssigneeDto> assignees;
 
-    public static ResponseIssueDto from(Issue issue) {
+    private ResponseIssueDto(Long id,
+                             String title,
+                             String author,
+                             int commentCount,
+                             String content,
+                             LocalDate createdAt,
+                             String issueStatus,
+                             Set<ResponseMilestoneDto> milestones,
+                             Set<ResponseCommentDto> comments,
+                             Set<ResponseLabelDto> labels,
+                             Set<ResponseAssigneeDto> assignees) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.commentCount = commentCount;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.issueStatus = issueStatus;
+        this.milestones = milestones;
+        this.comments = comments;
+        this.labels = labels;
+        this.assignees = assignees;
+    }
+
+    public static ResponseIssueDto of(Issue issue, Member member) {
+        Set<ResponseMilestoneDto> milestones = new HashSet<>();
+
+        if (issue.getMilestone() != null) {
+            milestones = Set.of(ResponseMilestoneDto.from(issue.getMilestone()));
+        }
+
+        return new ResponseIssueDto(
+                issue.getId(),
+                issue.getTitle(),
+                issue.getAuthorId(),
+                member.getProfileImageUrl(),
+                issue.getComments().size(),
+                issue.getContent(),
+                issue.getCreatedAt(),
+                issue.getStatus().toString().toLowerCase(),
+                milestones,
+                issue.getComments().stream()
+                        .map(ResponseCommentDto::from)
+                        .collect(Collectors.toSet()),
+                issue.getIssueLabels().stream()
+                        .map(IssueLabel::getLabel)
+                        .map(ResponseLabelDto::from)
+                        .collect(Collectors.toSet()),
+                issue.getIssueAssignees().stream()
+                        .map(IssueAssignee::getAssignee)
+                        .map(ResponseAssigneeDto::from)
+                        .collect(Collectors.toSet()));
+    }
+
+    public static ResponseIssueDto of(Issue issue) {
         Set<ResponseMilestoneDto> milestones = new HashSet<>();
 
         if (issue.getMilestone() != null) {
@@ -57,25 +112,4 @@ public class ResponseIssueDto {
                         .map(ResponseAssigneeDto::from)
                         .collect(Collectors.toSet()));
     }
-
-//    public static ResponseIssueDto from(Issue issue, Set<Label> label, Set<Assignee> assignee) {
-//        Set<ResponseMilestoneDto> milestones = new HashSet<>();
-//
-//        if (issue.getMilestone() != null) {
-//            milestones = Set.of(ResponseMilestoneDto.from(issue.getMilestone()));
-//        }
-//
-//        return new ResponseIssueDto(
-//                issue.getId(),
-//                issue.getTitle(),
-//                issue.getAuthorId(),
-//                issue.getComments().size(),
-//                issue.getContent(),
-//                issue.getCreatedAt(),
-//                issue.getStatus().toString().toLowerCase(),
-//                milestones,
-//                issue.getComments().stream().map(ResponseCommentDto::from).collect(Collectors.toSet()),
-//                label.stream().map(ResponseLabelDto::from).collect(Collectors.toSet()),
-//                assignee.stream().map(ResponseAssigneeDto::from).collect(Collectors.toSet()));
-//    }
 }

3569e7e99f4b6ce1695c3f57c909a08d15da7a97
Millie <jay.lee.2280@gmail.com>
2022-08-30 17:14:36 +0900

    [geombong/issue-tracker#27] Refactor: modify types in HeaderDropDown

--- FE/src/components/IssueList/IssueListHeader/HeaderDropDown/DetailsMenu/index.tsx
+++ FE/src/components/IssueList/IssueListHeader/HeaderDropDown/DetailsMenu/index.tsx
@@ -2,22 +2,28 @@ import { useRecoilState } from 'recoil';
 
 import * as S from '@/components/common/DropDown/style';
 import UserProfile from '@/components/common/UserProfile';
+import { FILTER } from '@/constants/constants';
 import { issueStatusState } from '@/stores/atoms/issue';
-import { getUniformMenus } from '@/utils/dropdown';
+import { getUniformMenus, UniformMenuType } from '@/utils/dropdown';
 
-const DetailsMenu = ({ title, filter, data }) => {
+type DetailsMenuProps = {
+  title: string;
+  filter: UniformMenuType;
+  data: any;
+};
+
+const DetailsMenu = ({ title, filter, data }: DetailsMenuProps) => {
   const [issueStatus, setIssueStatus] = useRecoilState(issueStatusState);
-
   const handleMenuClick = (name: string) => {
-    if (filter === 'author') {
+    if (filter === FILTER.author) {
       setIssueStatus({ ...issueStatus, author: name });
-    } else if (filter === 'label') {
+    } else if (filter === FILTER.label) {
       const currentLabels = issueStatus.label;
       setIssueStatus({
         ...issueStatus,
         label: [...currentLabels, name],
       });
-    } else if (filter === 'milestone') {
+    } else if (filter === FILTER.milestone) {
       setIssueStatus({
         ...issueStatus,
         milestone: name,
@@ -32,13 +38,20 @@ const DetailsMenu = ({ title, filter, data }) => {
 
   const menu = getUniformMenus(filter, data);
 
+  type MenuType = {
+    id: number;
+    name: string;
+    backgroundColor?: string;
+    image?: string;
+  };
+
   return (
     <S.DetailsMenu menuPosition="right" indicatorType="small">
       <S.DetailsMenuTitleWrapper>
         <S.DetailsMenuTitle indicatorType="small">{title}</S.DetailsMenuTitle>
       </S.DetailsMenuTitleWrapper>
       <ul>
-        {menu.map(({ id, name, backgroundColor, image }) => (
+        {menu.map(({ id, name, backgroundColor, image }: MenuType) => (
           <S.DetailsMenuItem key={id} indicatorType="small" onClick={() => handleMenuClick(name)}>
             <S.Menu>
               {image && <UserProfile imgUrl={image} userId={name} size="small" />}

46244fcfdfc3e65a6a9c19605fafdb803d07f5c8
Millie <jay.lee.2280@gmail.com>
2022-08-31 00:01:04 +0900

    [geombong/issue-tracker#26] Feat: implement login & sign up api

--- FE/src/mocks/handlers.ts
+++ FE/src/mocks/handlers.ts
@@ -1,8 +1,15 @@
 import AssigneesHandler from '@/mocks/Assignees';
 import IssueHandler from '@/mocks/Issues';
 import LabelsHandler from '@/mocks/Labels';
+import LoginHandler from '@/mocks/Login';
 import MilestonesHandler from '@/mocks/Milestones';
 
-const handlers = [...IssueHandler, ...LabelsHandler, ...MilestonesHandler, ...AssigneesHandler];
+const handlers = [
+  ...IssueHandler,
+  ...LabelsHandler,
+  ...MilestonesHandler,
+  ...AssigneesHandler,
+  ...LoginHandler,
+];
 
 export default handlers;

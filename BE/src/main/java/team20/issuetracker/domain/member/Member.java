5eec9a2edfb2797e488dceed0c5a1944e2e08711
geombong <shoy1415@gmail.com>
2022-09-26 14:34:56 +0900

    [geombong/issue-tracker#186] Edit: Modify Issue Entity
    Description
    - 이슈 엔티티와 멤버 엔티티 연관관계 매핑

--- BE/src/main/java/team20/issuetracker/domain/member/Member.java
+++ BE/src/main/java/team20/issuetracker/domain/member/Member.java
@@ -1,16 +1,22 @@
 package team20.issuetracker.domain.member;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team20.issuetracker.domain.issue.Issue;
 import team20.issuetracker.login.oauth.Role;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,6 +31,9 @@ public class Member {
     private String name;
     private String email;
     private String profileImageUrl;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "member")
+    private List<Issue> issues = new ArrayList<>();
 
     @Enumerated(EnumType.STRING)
     private Role role;

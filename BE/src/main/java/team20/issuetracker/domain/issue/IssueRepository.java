be7212390657924abc4089324c09213461c77a49
geombong <shoy1415@gmail.com>
2022-10-15 00:49:29 +0900

    [geombong/issue-tracker#203] edit: IssueRepository
    Description
    - 페이징 처리를 위해 IssueRepository 수정

--- BE/src/main/java/team20/issuetracker/domain/issue/IssueRepository.java
+++ BE/src/main/java/team20/issuetracker/domain/issue/IssueRepository.java
@@ -1,5 +1,7 @@
 package team20.issuetracker.domain.issue;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +15,10 @@ public interface IssueRepository extends JpaRepository<Issue, Long> {
     @Query("select i from Issue i where i.authorId = :oauthId")
     List<Issue> findAllMyIssues(@Param("oauthId") String oauthId);
 
-    @Query("select i from Issue i join fetch i.member m join fetch i.milestone mi")
-    List<Issue> findAllIssue();
+    @Query("select count(i) from Issue i")
+    long findByIssueCount();
+
+    @Query(value = "select i from Issue i left join fetch i.member m left join fetch i.milestone mi where i.status = :status",
+            countQuery = "select count(i) from Issue i left join i.member left join i.milestone where i.status = :status")
+    Page<Issue> findIssues(Pageable pageable, @Param("status") IssueStatus status);
 }

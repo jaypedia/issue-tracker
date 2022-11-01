dabe646666d1b4c06d34f97a617c45ee8ce2cfbe
Millie <jay.lee.2280@gmail.com>
2022-11-01 13:55:50 +0900

    [#231] Feat: implement pagination in Home page

--- FE/src/components/IssueList/index.tsx
+++ FE/src/components/IssueList/index.tsx
@@ -1,15 +1,68 @@
+import ReactPaginate from 'react-paginate';
+import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
+
 import IssueContainer from './IssueContainer';
 import IssueListHeader from './IssueListHeader';
 
+import Loading from '@/components/common/Loading';
+import { useGetIssue } from '@/hooks/useIssue';
+import { issueStatusState } from '@/stores/atoms/issue';
+import { COLOR } from '@/styles/color';
 import { ListContainer } from '@/styles/list';
-import { IssuesDataType } from '@/types/issueTypes';
 
-const IssueList = ({ issues, openIssueCount, closedIssueCount }: IssuesDataType) => {
-  return (
-    <ListContainer>
-      <IssueListHeader openIssueCount={openIssueCount} closedIssueCount={closedIssueCount} />
-      <IssueContainer issues={issues} />
-    </ListContainer>
+const Pagination = styled(ReactPaginate)`
+  display: flex;
+  justify-content: center;
+  color: ${COLOR.grey[400]};
+
+  & > * {
+    padding: 10px;
+
+    :hover {
+      border: 1px solid ${COLOR.grey[200]};
+      border-radius: 10px;
+    }
+  }
+
+  .currentPage {
+    color: white;
+    background-color: ${COLOR.primary[300]};
+    border-radius: 10px;
+  }
+`;
+
+const IssueList = () => {
+  const [issueStatus, setIssueStatus] = useRecoilState(issueStatusState);
+  const { data, isLoading } = useGetIssue(issueStatus);
+
+  const handlePageClick = (e: { selected: number }) => {
+    const page = e.selected + 1;
+    setIssueStatus({ ...issueStatus, page });
+  };
+
+  return isLoading || !data ? (
+    <Loading />
+  ) : (
+    <>
+      <ListContainer>
+        <IssueListHeader
+          openIssueCount={data.openIssueCount}
+          closedIssueCount={data.closedIssueCount}
+        />
+        <IssueContainer issues={data.issues} />
+      </ListContainer>
+      <Pagination
+        pageCount={data.pageable.totalPages}
+        pageRangeDisplayed={10}
+        breakLabel="..."
+        previousLabel="<"
+        nextLabel=">"
+        onPageChange={handlePageClick}
+        activeClassName="currentPage"
+        forcePage={issueStatus.page - 1}
+      />
+    </>
   );
 };
 
